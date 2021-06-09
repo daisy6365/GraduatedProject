bf356d2f73e74a38d5c840e4f61e3e2c17ac05b1
daisy <gksekqls9808@inu.ac.kr>
2021-06-10 03:19:19 +0900

    로그인 뷰 생성

--- app/src/main/java/com/example/graduatedproject/Fragment/MyPage.kt
+++ app/src/main/java/com/example/graduatedproject/Fragment/MyPage.kt
@@ -1,6 +1,8 @@
 package com.example.graduatedproject.Fragment
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,42 +17,30 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  * Use the [MyPage.newInstance] factory method to
  * create an instance of this fragment.
+ * var pref = getSharedPreferences(PREFERENCE, MODE_PRIVATE)
+var token:String = pref.getString("userToken", "").toString()
  */
 class MyPage : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    val PREFERENCE = "SharedPreference"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        return inflater.inflate(R.layout.before_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPage.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                MyPage().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("Life_cycle", "onViewCreated")
+        super.onViewCreated(view, savedInstanceState)
+
+
+
     }
+
 }
